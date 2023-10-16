dcd71a89e3049a32c06808596b80d62df5401e4a
Rehan Chalana <rehanchalanaprsnl@gmail.com>
2023-10-16 18:59:01 +0530

    completed initial code for Percolation.java

--- Percolation.java
+++ Percolation.java
@@ -7,11 +7,13 @@ public class Percolation {
     private WeightedQuickUnionUF UF;
     private boolean[] openOrNot;
     private int N;
+    private int numberOfOpenSites;
 
     public Percolation(int n){
         this.N=n;
         this.UF = new WeightedQuickUnionUF((n*n)+2);
         this.openOrNot = new boolean[n*n];
+        numberOfOpenSites = 0;
         for(int i=0;i<openOrNot.length;i++){
             openOrNot[i]=false;
             if(i<n){
@@ -26,21 +28,38 @@ public class Percolation {
     public void open(int row, int col){
         if(!openOrNot[convertIndex(row, col)-1]){
             unionNBRS(row,col);
-            openOrNot[convertIndex(row, col)-1]=true;;
+            openOrNot[convertIndex(row, col)-1]=true;
+            numberOfOpenSites++;
         }     
+    }
+
+    public boolean isOpen(int row , int col){
+        return openOrNot[convertIndex(row, col)-1];
+    }
+
+    public boolean isFull(int row , int col){
+        return UF.find(convertIndex(row, col))==UF.find(0);
+    }
+
+    public int numberOfOpenSites(){
+        return numberOfOpenSites;
+    }
+
+    public boolean percolates(){
+        return UF.find(N*N+2)==UF.find(0);
     }
 
     private void unionNBRS(int row , int col){
         if(checkRight(row,col)){
             UF.union(convertIndex(row, col),convertIndex(row, col)+1);
         }
-        if(checkLeft()){
+        if(checkLeft(row,col)){
             UF.union(convertIndex(row, col),convertIndex(row, col)-1);
         }
-        if(checkUp()){
+        if(checkUp(row,col)){
            UF.union(convertIndex(row, col),convertIndex(row, col)-5); 
         }
-        if(checkDown()){
+        if(checkDown(row,col)){
             UF.union(convertIndex(row, col),convertIndex(row, col)+5); 
         }
     }
@@ -50,7 +69,38 @@ public class Percolation {
     }
 
     private boolean checkRight(int row , int col){
-        
+        if(convertIndex(row, col)+1<=row*N){
+            if(isOpen(row, col+1)){
+                return true;
+            }   
+        }
+        return false;
+    }
+
+    private boolean checkLeft(int row, int col){
+        if(convertIndex(row, col)-1>=(5*(row-1))+1){
+            if(isOpen(row, col-1)){
+                return true;
+            }    
+        }
+        return false;
+    }
+
+    private boolean checkUp(int row,int col){
+        if(convertIndex(row, col)-5>=1){
+            if(isOpen(row-1, col)){
+                return true;
+            }    
+        }
+        return false;
+    }
+
+    private boolean checkDown(int row , int col){
+        if(convertIndex(row, col)+5>=N*N){
+            if(isOpen(row+1, col))
+            return true;
+        }
+        return false;
     }
 
 
