5953b24724d4f88da7dba9502c8ed497c7f052f5
RehanChalana <rehanchalanaprsnl@gmail.com>
2023-11-03 22:16:46 +0530

    added initial implementation of Permutation.java

--- week2/Permutation.java
+++ week2/Permutation.java
@@ -1,6 +1,15 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 public class Permutation {
     public static void main(String[] args) {
-        
+        RandomizedQueue<String> rqStrings = new RandomizedQueue<>();
+        while(StdIn.hasNextLine()){
+            rqStrings.enqueue(StdIn.readLine());
+        }
+        for(int i=0;i<Integer.valueOf(args[0]);i++){
+            StdOut.print(rqStrings.sample());
+        }
     }
     
 }
