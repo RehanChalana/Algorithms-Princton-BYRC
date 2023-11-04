a491c02c33deefe604ac83f4f7dfef16f08f4b54
RehanChalana <rehanchalanaprsnl@gmail.com>
2023-11-04 23:35:22 +0530

    96 points!!

--- week2/Permutation.java
+++ week2/Permutation.java
@@ -4,12 +4,12 @@ import edu.princeton.cs.algs4.StdRandom;
 public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> rqStrings = new RandomizedQueue<>();
-        while(StdIn.isEmpty()){
+        while(!StdIn.isEmpty()){
             rqStrings.enqueue(StdIn.readString());
         }
-        
         for(int i=0;i<Integer.parseInt(args[0]);i++){
-            StdOut.print(rqStrings.sample());
+            // StdOut.print(rqStrings.dequeue());
+            StdOut.println(rqStrings.dequeue());
         }
     }
     
