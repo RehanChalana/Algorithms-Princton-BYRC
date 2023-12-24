a1dd509a062ef6fd276a70d454136b62dcc7cd76
RehanChalana <rehanchalanaprsnl@gmail.com>
2023-12-24 12:31:08 +0530

    fix points array needed to be sorted first

--- week3/BruteCollinearPoints.java
+++ week3/BruteCollinearPoints.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BruteCollinearPoints {
     private ArrayList<LineSegment> lines;
 
     public BruteCollinearPoints(Point[] points){
         lines = new ArrayList<>();
+        Arrays.sort(points);
         for(int p=0;p<points.length-3;p++){
             for(int q=p+1;q<points.length-2;q++){
                 for(int r=q+1;r<points.length-1;r++){
                     for(int s=r+1;s<points.length;s++){
                         if(points[p].slopeTo(points[q])==points[p].slopeTo(points[r]) && points[p].slopeTo(points[r])==points[p].slopeTo(points[s])){
                             lines.add(new LineSegment(points[p],points[s]));
-                            // System.out.println(points[p]+"    "+points[s]);
-                            // p++;
                         }
                     }
                 }
