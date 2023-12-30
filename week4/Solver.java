befaa11e850fc72bf349101660c2913d47748bf0
RehanChalana <rehanchalanaprsnl@gmail.com>
2023-12-30 20:13:35 +0530

    implemented constructor and comparator methods!

--- week4/Solver.java
+++ week4/Solver.java
@@ -1,22 +1,69 @@
 import java.util.Comparator;
+import java.util.Scanner;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 
 public class Solver {
-    private MinPQ minPQ; 
+    private MinPQ<Node> minPQ; 
+    private Node iniNode;
+    public static final Comparator<Node> BY_BOARD = new byBoard(); 
     
     private class Node{
         Board board;
         int moves;
         Node previous;
-        
-        public int compareTo(Node that){
-            return (this.board.manhattan()+this.moves)-(that.board.manhattan()+that.moves);
-        }
-        
     }
 
-    
+    private static class byBoard implements Comparator<Node>{
+            public int compare(Node x , Node y){
+                return (x.board.manhattan()+x.moves)-(y.board.manhattan()+y.moves);
+            }
+    }
+
+    public Solver(Board initial){
+        this.minPQ = new MinPQ<>(BY_BOARD);
+        iniNode = new Node();
+        iniNode.board=initial;
+        iniNode.moves=0;
+        iniNode.previous=null;
+        this.minPQ.insert(iniNode);
+        int count = 0;
+        while(true){
+            Node rmNode = this.minPQ.delMin();
+            System.out.println(rmNode.board);
+            if(rmNode.board.isGoal()){
+                break;
+            }
+            count++;
+            for(Board i :rmNode.board.neighbors()){
+                Node newNode = new Node();
+                newNode.board=i;
+                newNode.moves=count;
+                newNode.previous=rmNode;
+                this.minPQ.insert(newNode);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        In in = new In(scan.nextLine());
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                tiles[i][j] = in.readInt();
+            }
+        }        
+        Board initial = new Board(tiles);
+        Solver solve = new Solver(initial);
+
+    }
+
+
+
+
 
 
 }
