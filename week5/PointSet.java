86c062876da3761efa6d7dd637cf04bc7633e094
RehanChalana <rehanchalanaprsnl@gmail.com>
2024-01-12 21:26:28 +0530

    implemented nearest

--- week5/PointSet.java
+++ week5/PointSet.java
@@ -32,10 +32,23 @@ public class PointSet {
         }
     }
 
+    public Point2D nearest(Point2D p){
+        Point2D nearest=this.BST.first();
+        for(Point2D i:this.BST){
+            if(p.distanceTo(i)<p.distanceTo(nearest)){
+                nearest = i;
+            }
+        }
+        return nearest;
+    }
+
     public static void main(String[] args) {
         PointSet set = new PointSet();
         set.insert(new Point2D(0.4, 0.9));
-        set.insert(new Point2D(0.1, 0.5));
+        set.insert(new Point2D(0.5, 0.5));
+        set.insert(new Point2D(0.2, 0.2));
+        set.insert(new Point2D(0.1, 0));
+        System.out.println(set.nearest(new Point2D(0, 0)));
         set.draw();
     }
 }
