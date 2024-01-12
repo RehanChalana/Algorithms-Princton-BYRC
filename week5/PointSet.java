3a02b394ab99da446ef13aa49acc40bd813afd1a
RehanChalana <rehanchalanaprsnl@gmail.com>
2024-01-12 21:43:49 +0530

    implemented PointSet().java

--- week5/PointSet.java
+++ week5/PointSet.java
@@ -1,6 +1,8 @@
 import java.util.TreeSet;
 
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdDraw;
 public class PointSet {
     private TreeSet<Point2D> BST;
@@ -42,13 +44,27 @@ public class PointSet {
         return nearest;
     }
 
+    public Iterable<Point2D> range(RectHV rect){
+        Stack<Point2D> stack = new Stack<>();
+        for(Point2D i:this.BST){
+            if(i.x()>=rect.xmin() && i.x()<=rect.xmax() && i.y()>=rect.ymin() && i.y()<=rect.ymax()){
+                stack.push(i);
+            }
+        }
+        return stack;
+    }
+
     public static void main(String[] args) {
         PointSet set = new PointSet();
-        set.insert(new Point2D(0.4, 0.9));
-        set.insert(new Point2D(0.5, 0.5));
-        set.insert(new Point2D(0.2, 0.2));
-        set.insert(new Point2D(0.1, 0));
-        System.out.println(set.nearest(new Point2D(0, 0)));
+        set.insert(new Point2D(0.4, 0.4));
+        set.insert(new Point2D(0.2, 0.4));
+        set.insert(new Point2D(0.7, 0.8));
+        set.insert(new Point2D(0.9, 0.9));
+        for(Point2D i:set.range(new RectHV(0.1, 0.1, 0.6, 0.6))){
+            System.out.println(i);
+        }
+        // System.out.println(set.nearest(new Point2D(0, 0)));
+        // set.insert(new Point2D(0.0, 0.0));
         set.draw();
     }
 }
