f6167595cb33e1fb035be23b6e1a2fbdbbe21936
RehanChalana <rehanchalanaprsnl@gmail.com>
2024-01-13 14:27:21 +0530

    implemented contains() method

--- week5/KdTree.java
+++ week5/KdTree.java
@@ -5,17 +5,19 @@ public class KdTree{
     private Node root;
     private int size;
 
+    public KdTree(){
+        this.size = 0;
+    }
+
     private static class Node{
         private Point2D p;
         private RectHV rect;
         private Node lb;
         private Node rt;
         // false for y and true for x
-        private boolean orien;
 
-        public Node(Point2D p,boolean orien){
+        public Node(Point2D p){
             this.p = p;
-            this.orien = orien;
         }
     }
 
@@ -34,21 +36,64 @@ public class KdTree{
     private Node insert(Node cur,Point2D p,boolean orien){
         if(cur==null){
             this.size++;
-            return new Node(p, orien);
+            return new Node(p);
         }
         double cmp;
-        if(cur.orien){
+        if(orien){
              cmp = p.x()-cur.p.x();
         } else{
              cmp = p.y()-cur.p.y();
         }
         if(cmp<0){
-            cur.lb = insert(cur.lb, p, !cur.orien);
+            cur.lb = insert(cur.lb, p, !orien);
         }
         else if(cmp>=0){
-            cur.rt = insert(cur.rt, p, !cur.orien);
+            cur.rt = insert(cur.rt, p, !orien);
         }
         return cur;
+    }
+
+    public boolean contains(Point2D p){
+        return contains(this.root, p,true);
+    }
+
+    private boolean contains(Node cur,Point2D p,boolean orien){
+        if(cur==null){
+            return false;
+        }
+        if(cur.p.equals(p)){
+            return true;
+        }
+        double cmp;
+        if(orien){
+             cmp = p.x()-cur.p.x();
+        } else{
+             cmp = p.y()-cur.p.y();
+        }
+        if(cmp<0){
+            return contains(cur.lb,p,!orien);
+        } else if(cmp>=0){
+            return contains(cur.rt,p,!orien);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        KdTree tree = new KdTree();
+        tree.insert(new Point2D(0.1, 0.2));
+        tree.insert(new Point2D(0.4, 0.6));
+        tree.insert(new Point2D(0.5, 0.8));
+        tree.insert(new Point2D(0.25, 0.67));
+        tree.insert(new Point2D(0.10, 0));
+        tree.insert(new Point2D(0.6, 0.69));
+        System.out.println(tree.contains(new Point2D(0.1, 0.2)));
+        System.out.println(tree.contains(new Point2D(0.4, 0.6)));
+        System.out.println(tree.contains(new Point2D(0.5, 0.8)));
+        System.out.println(tree.contains(new Point2D(0.25, 0.67)));
+        System.out.println(tree.contains(new Point2D(0.10, 0)));
+        System.out.println(tree.contains(new Point2D(0.6, 0.69)));
+        System.out.println(tree.contains(new Point2D(1, 0.5)));
+
     }
 
 }
